9eed9ca32a8ed1b0de18e39ca4d0d6278916cfba
DonaldKisaka <davidkisaka4@gmail.com>
2026-04-09 17:29:29 +0300

    Initial commit

---
+++ spring-ai-starter/src/test/java/com/donaldkisaka/spring_ai_starter/SpringAiStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donaldkisaka.spring_ai_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
